87fe87c482f6f356fef440aa6c3e78f9a545d0c4
Mwitishi <mwitishi@gmail.com>
2013-02-15 13:36:21 +0100

    Added main function skeleton

--- zombie.c
+++ zombie.c
@@ -2,5 +2,28 @@
 
 int main(int argc,char **argv)
 {
+    //Print game data
+    printf("%s %s\n",ZOMBIE_NAME,ZOMBIE_VERSION);
+
+    //If arguments passed
+    if(argc>1)
+    {
+        //Show game information
+        if(strcmp(argv[1],"info")==0)
+            printf("%s is an action game in 2D about killing zombies.\n",ZOMBIE_NAME);
+
+        return 0;
+    }
+
+    //SDL initializing,check for error
+    if(SDL_Init(SDL_INIT_EVERYTHING)!=0)
+    {
+        printf("Error during SDL initializing");
+        return 1;
+    }
+
+    //SDL closing
+    SDL_Quit();
+
     return 0;
 }
--- zombie.h
+++ zombie.h
@@ -2,5 +2,9 @@
 #define ZOMBIE_H
 
 #include <stdio.h>
+#include <SDL/SDL.h>
+
+#define ZOMBIE_NAME "Zombie by Mwitishi"
+#define ZOMBIE_VERSION "v0.0"
 
 #endif
