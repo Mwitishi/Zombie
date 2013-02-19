04524812d0da693f2f239f56097124ec0a4c57b1
Mwitishi <mwitishi@gmail.com>
2013-02-19 17:21:22 +0100

    Added zia_clear, previous position arrays

--- zia.c
+++ zia.c
@@ -10,8 +10,8 @@
 #include "zombie.h"
 
 //IA variables
-float *prevx;
-float *prevy;
+float *prevx = NULL;
+float *prevy = NULL;
 
 //Main IA function, runs all others
 int zia_run()
@@ -29,5 +29,36 @@ int zia_run()
 
 int zia_init()
 {
+    int i1;
+
+    //Allocate array
+    prevx = (float*) malloc(sizeof(float) * ZOMBIE_ZOMBIE_QUAN);
+    if(prevx == NULL) return 1;
+
+    //Initialize array empty
+    for(i1 = 0 ; i1 < ZOMBIE_ZOMBIE_QUAN ; i1++)
+        prevx[i1] = -1;
+
+    //Allocate array
+    prevy = (float*) malloc(sizeof(float) * ZOMBIE_ZOMBIE_QUAN);
+    if(prevy == NULL) return 1;
+
+    return 0;
+}
+
+int zia_clear()
+{
+    //Free array
+    if(prevx != NULL) {
+        free(prevx);
+        prevx = NULL;
+    }
+
+    //Free array
+    if(prevy != NULL) {
+        free(prevy);
+        prevy = NULL;
+    }
+
     return 0;
 }
