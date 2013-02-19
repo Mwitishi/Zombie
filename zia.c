5461c681e83e0bdc232b0e95c8c5a8896f5476a8
Mwitishi <mwitishi@gmail.com>
2013-02-19 16:58:10 +0100

    Added zia.c

---
+++ zia.c
@@ -0,0 +1,8 @@
+/*
+ *  File: zia.c / Project: Zombie
+ *  Author: Mwitishi
+ *  This file contains functions for the IA
+ *  of the zombies in game Zombie.
+ *  Macros, declarations & zent structure
+ *  are in zombie.h.
+ */
